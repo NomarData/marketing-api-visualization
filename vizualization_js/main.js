02fcc776be13a4c3392a0bdb85483512fc6771ab
Matheus Araujo <maraujo@qf.org.qa>
2016-12-20 11:02:45 +0300

    Add scale

--- vizualization_js/main.js
+++ vizualization_js/main.js
@@ -72,18 +72,34 @@ $(document).ready(function () {
             btnsTopicsSelectors.init();
         });
     });
+    //----------------------------------------------------------------------------
 
 
+    function render(scale,selector) {
+        var max=1, data = [], min=-1;
+        var step = (max-min)/20;
+        for (var i=-1.0 + step;i<max;i=i+step){
+            data.push(i);
+        }
 
-    // NODES_SELECTED.selectDefaultCountries();
+        var selection =  d3.select(selector).selectAll("div.cell").data(data);
+        selection.enter()
+            .append("div")
+            .classed("cell",true)
+            .append("div.test")
+            .append("span");
+        selection.data(data).exit().remove();
+        selection.style("display","inline-block")
+            .style("background-color",function(d){
+                return scale(d);
+            })
+            .select("span")
+                .text(function(d,i){return d.toFixed(2);});
 
-    //
-    //
-    //
-    // // CountriesBarCharts();
-    // // CountriesBarCharts2();
-    //
-    // var instances = fusionAPI.getDefaultData();
+
+    }
+    render(colorFunction, "#color");
+
 
 
 });
