7cf5068e5b28a59839f54657a214457ccdd5e745
Matheus Araujo <maraujo@qf.org.qa>
2016-12-22 10:07:33 +0300

    Small fixes and testing with qatar/bahrein zoom

--- vizualization_js/utils.js
+++ vizualization_js/utils.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     colorRangeScale = ["#d73027", "#fc8d59", "#fee08b", '#ffffbf', '#d9ef8b', '#91cf60', '#1a9850'];
     colorD3RangeScale = [d3.rgb("#d73027"), d3.rgb("#fc8d59"), d3.rgb("#fee08b"), d3.rgb('#ffffbf'), d3.rgb('#d9ef8b'), d3.rgb('#91cf60'), d3.rgb('#1a9850')];
     domainLinear = [-1, -0.66, -0.33, 0, 0.33, 0.66, 1];
-    domainNotLinear = [-1, -0.15, -0.05, 0, 0.05, 0.15, 1];
+    domainNotLinear = [-0.7, -0.3, -0.05, 0, 0.05, 0.3, 0.7];
     breakPointsColor = buildBreakPoints(domainLinear, colorRangeScale);
     colorFunction = d3.scale.linear().domain(domainNotLinear).interpolate(d3.interpolateRgb).range(colorD3RangeScale);
     colorNotLinearFunction = d3.scale.linear().domain(domainNotLinear).interpolate(d3.interpolateRgb).range(colorD3RangeScale);
