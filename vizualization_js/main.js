9282d32731f5d44380f8a8956c611873fd665601
Matheus Araujo <maraujo@qf.org.qa>
2016-12-27 10:46:26 +0300

    Testing Social Media Buttons

--- vizualization_js/main.js
+++ vizualization_js/main.js
@@ -61,26 +61,5 @@ $(document).ready(function () {
             sharebleLink.init();
         });
     });
-
-
-    $.ajaxSetup({ cache: true });
-    $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
-        FB.init({
-            appId: '358697700',
-            version: 'v2.7' // or v2.1, v2.2, v2.3, ...
-        });
-    });
-    $("#fb-share-button").click(function(){
-        FB.ui({
-            method: 'share',
-            display: 'popup',
-            href: 'https://developers.facebook.com/docs/',
-        }, function(response){});
-    });
-    FB.ui({
-        method: 'share',
-        display: 'popup',
-        href: 'https://developers.facebook.com/docs/',
-    }, function(response){});
 });
 
